d9d7c773ce320cc1fa9494a906d75e8127a6655c
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-21 22:26:28 +0000

    updated event listeners to account for copy/paste

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  $("#tweet-text").on('keydown', function() {
-    let charCount = 140 - this.value.length;
+  const setCounterColor = function() {
+    let charCount = 140 - $(this).val().length;
     if (charCount < 0) {
       $(this).siblings('.counter').text(charCount).css('color','red');
     } else {
       $(this).siblings('.counter').text(charCount).css('color','#545149');
     }
-  });
+  }
+  $("#tweet-text").on('keydown', setCounterColor);
+  $("#tweet-text").on('keyup', setCounterColor);
+  $("#tweet-text").on('paste', setCounterColor);
 })
