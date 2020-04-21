fdbfda90012dc79157465bf0ba9b0c9d0d0678f5
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-21 03:26:33 +0000

    Refined spacing and tabs

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -22,7 +22,6 @@ module.exports = function(DataHelpers) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
-
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
     const tweet = {
       user: user,
@@ -36,7 +35,7 @@ module.exports = function(DataHelpers) {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
-        res.status(201).send();
+        res.status(201).render();
       }
     });
   });
