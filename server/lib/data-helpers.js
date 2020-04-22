88f89c1c240114d275b7fcd96c90669ed4311d21
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-22 22:37:26 +0000

    Accidentally changed, reset to original

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,7 +10,7 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.unshift(newTweet);
+        db.tweets.push(newTweet);
         callback(null, true);
       });
     },
