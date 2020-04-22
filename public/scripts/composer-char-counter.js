e1f9189fd9ef9000a15c0fd199eadb7d488b590b
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-22 22:35:11 +0000

    Updated to reset count when form is submitted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,7 +7,12 @@ $(document).ready(function() {
       $(this).siblings('.counter').text(charCount).css('color','#545149');
     }
   }
+  // Accounts for different input situations
   $("#tweet-text").on('keydown', setCounterColor);
   $("#tweet-text").on('keyup', setCounterColor);
   $("#tweet-text").on('paste', setCounterColor);
+  // resets counter to 140 when tweet is submitted
+  $('form').submit(function() {
+    $(this).children('.counter').text(140);
+  });
 })
