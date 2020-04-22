eb28d2578e886480bbcf5f17f8533c14b85d6108
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-22 22:34:44 +0000

    refactored tweet template to its own file

---
+++ public/scripts/new-tweet-template.js
@@ -0,0 +1,18 @@
+// Creates a new instance of a tweet to be added to the tweet-container
+
+const createTweetElement = function(data) {
+  const time = formatDate(data.created_at);
+  let newTweet = `<article class="tweet">
+    <header>
+      <img class="hover-effect" src=${data.user.avatars}>
+      <h5 class="name hover-effect">${data.user.name}<h5>
+      <h5 class="tag">${data.user.handle}</h5>
+    </header>
+    <span class="message hover-effect">${data.content.text}</span>
+    <footer>
+      <span class="date hover-effect">${time}</span>
+    </footer>
+  </article>`
+
+  return $(newTweet);
+}
