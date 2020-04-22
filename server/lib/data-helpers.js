a590bd39f744ef0913a1a3be0efd0daa0e291126
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-22 22:06:49 +0000

    updated saveTweet to add new tweets to the front of the array so that a new tweet is displayed at the top of the tweet container

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,7 +10,7 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.tweets.unshift(newTweet);
         callback(null, true);
       });
     },
