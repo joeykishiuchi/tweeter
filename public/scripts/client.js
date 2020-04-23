6e93195096a55ff118f6bb5353faa22bae839ed1
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-23 02:55:54 +0000

    implemented character limit error message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,7 +13,7 @@ const renderTweets = function(database) {
   for (const data of database) {
     const $tweet = createTweetElement(data);
     // Adds new tweets to the top of the container
-    $('#tweet-container').prepend($tweet);
+    $('#tweet-container').prepend($tweet).slideDown();
   }
 };
 
@@ -30,7 +30,7 @@ $("form").submit(function(event) {
   event.preventDefault();
   const charCounter = $(this).children('.counter').text();
   if (Number(charCounter < 0)) {
-      alert('That\'s too many characters');
+    $('#char-limit-err').slideToggle('slow');
   } else {
     $.post('/tweets', $(this).serialize())
     .then(function() {
@@ -38,7 +38,7 @@ $("form").submit(function(event) {
       $('#tweet-container').empty();
       loadTweets();
     });
-    
+    $('#char-limit-err').slideUp('slow');
     // Resets counter to 140 when tweet is submitted
     $(this).children('.counter').text(140);
     // Resets input to empty when form is submitted
