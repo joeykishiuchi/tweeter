1931d3eb14d9bc67e4a7e987971811b0efe906cd
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-04-24 04:08:24 +0000

    included moment js for timestamp on tweets

--- public/scripts/new-tweet-template.js
+++ public/scripts/new-tweet-template.js
@@ -1,27 +1,27 @@
 // formats timestamp to date
-const formatDate = function(timestamp) {
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemebr', 'December' ];
-  const month = new Date(timestamp).getMonth();
-  const day = new Date(timestamp).getUTCDate();
-  const year = new Date(timestamp).getFullYear();
-  return `${months[month]} ${day}, ${year}`;
-};
+// const formatDate = function(timestamp) {
+//   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemebr', 'December' ];
+//   const month = new Date(timestamp).getMonth();
+//   const day = new Date(timestamp).getUTCDate();
+//   const year = new Date(timestamp).getFullYear();
+//   return `${months[month]} ${day}, ${year}`;
+// };
 
 // Creates a new instance of a tweet to be added to the tweet-container
 const createTweetElement = function(data) {
-  const time = formatDate(data.created_at);
+  const date = new Date(data.created_at);
+  const timeAgo = moment(date).fromNow();
   let newTweet = `<article class="tweet">
     <header>
       <img class="hover-effect" src=${data.user.avatars}>
       <h5 class="name hover-effect">${data.user.name}<h5>
       <h5 class="tag">${data.user.handle}</h5>
     </header>
-    <span class="message hover-effect">${escape(data.content.text)}</span>
+    <p class="message hover-effect">${escape(data.content.text)}</p>
     <footer>
-      <span class="date hover-effect">${time}</span>
+      <span class="date hover-effect">${timeAgo}</span>
       <span class="reaction-icons hover-effect">&#9873; &#8630; &#9829;</span>
     </footer>
   </article>`
-
   return $(newTweet);
 }
